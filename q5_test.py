a11f5a8f9792435065c64f027bee36c96c5de336
Doğa Kukul <72515524+dkukul19@users.noreply.github.com>
2025-11-22 23:53:26 +0300

    Update q5_test.py

--- q5_test.py
+++ q5_test.py
@@ -1,26 +1,26 @@
 import unittest
-from q5 import longest_consecutive_subsequence_len
+from q5 import hidden_sequence_len
 
 
-class TestFindLongestConsecutiveSubsequence(unittest.TestCase):
+class TestFindHidden_sequence_len(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(longest_consecutive_subsequence_len([10, 4, 20, 1, 3, 2, 5]), 5, "Should be 5")
+        self.assertEqual(hidden_sequence_len([10, 4, 20, 1, 3, 2, 5]), 5, "Should be 5")
 
     def test_2(self):
-        self.assertEqual(longest_consecutive_subsequence_len([1, 2, 3, 4, 5]), 5, "Should be 5")
+        self.assertEqual(hidden_sequence_len([1, 2, 3, 4, 5]), 5, "Should be 5")
 
     def test_3(self):
-        self.assertEqual(longest_consecutive_subsequence_len([]), 0, "Should be 0")
+        self.assertEqual(hidden_sequence_len([]), 0, "Should be 0")
 
     def test_4(self):
-        self.assertEqual(longest_consecutive_subsequence_len([1, 1, 2, 2, 3, 3, 4, 4]), 4, "Should be 4")
+        self.assertEqual(hidden_sequence_len([1, 1, 2, 2, 3, 3, 4, 4]), 4, "Should be 4")
 
     def test_5(self):
-        self.assertEqual(longest_consecutive_subsequence_len([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 10, "Should be 10")
+        self.assertEqual(hidden_sequence_len([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 10, "Should be 10")
 
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestFindLongestConsecutiveSubsequence)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFindHidden_sequence_len)
     result = unittest.TextTestRunner().run(suite)
     total_tests_run = result.testsRun
     total_failures = len(result.failures) + len(result.errors)
